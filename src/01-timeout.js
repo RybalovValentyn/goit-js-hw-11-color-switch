5d9a18e6b47682943a341eed2d261d5762a612df
RybalovValentyn <0661629069@ukr.net>
2021-06-06 21:20:41 +0300

    1

---
+++ src/01-timeout.js
@@ -0,0 +1,31 @@
+
+// const logger = time => {
+//   console.log(`Лог через ${time}ms, потому что не отменили таймаут`);
+// };
+
+// const timerId = setTimeout(logger, 2000, 2000);
+
+// console.log(timerId);
+
+// const shouldCancelTimer = Math.random() > 0.3;
+// console.log(shouldCancelTimer);
+
+// if (shouldCancelTimer) {
+//   clearTimeout(timerId);
+// }
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+
+console.log(btnStart);
+
+console.log(btnStop);
