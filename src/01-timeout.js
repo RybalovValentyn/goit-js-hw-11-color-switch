5fbf2153f52b5382e9ec8c71481622daa8bfe3a1
RybalovValentyn <0661629069@ukr.net>
2021-06-13 11:55:46 +0300

    home 1-1

--- src/01-timeout.js
+++ src/01-timeout.js
@@ -1,19 +1,4 @@
 
-// const logger = time => {
-//   console.log(`Лог через ${time}ms, потому что не отменили таймаут`);
-// };
-
-// const timerId = setTimeout(logger, 2000, 2000);
-
-// console.log(timerId);
-
-// const shouldCancelTimer = Math.random() > 0.3;
-// console.log(shouldCancelTimer);
-
-// if (shouldCancelTimer) {
-//   clearTimeout(timerId);
-// }
-
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -25,7 +10,42 @@ const colors = [
 
 const btnStart = document.querySelector('[data-action="start"]');
 const btnStop = document.querySelector('[data-action="stop"]');
+const bodyColor = document.querySelector('body');
+let isRunToogleColor = false;
+let isStopped = false;
+let intervalId = null;
 
-console.log(btnStart);
+btnStart.addEventListener('click', bodyColorToogle);
+btnStop.addEventListener('click',toogleColorStop)
 
-console.log(btnStop);
+const logger = function bodyTime() {
+  console.log('dsgdsg');
+if (isStopped === false) {
+  let bodyToogleColor = colors[randomIntegerFromInterval()]
+  console.log(bodyToogleColor);
+  bodyColor.style.backgroundColor = bodyToogleColor;
+  isRunToogleColor = true;
+};
+};
+
+function bodyColorToogle() {
+   isStopped = false;
+  console.log(isRunToogleColor);
+    if (isRunToogleColor ===false){ 
+     intervalId = setInterval(logger,1000);
+          console.log('is run');
+      };
+};
+
+function toogleColorStop() {
+  clearInterval(intervalId);
+   isStopped = true;
+   isRunToogleColor =false
+          };
+
+const randomIntegerFromInterval = (min, max) => {
+  min = 0;
+  max = colors.length -1;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
